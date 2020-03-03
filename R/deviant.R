b5ad638b55a930e18fa310d6d50b947fe309a3a7
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2020-03-03 23:02:59 +0100

    Added most overpredicted case

--- R/deviant.R
+++ R/deviant.R
@@ -12,11 +12,32 @@
 #' @examples
 #' df <- lm(mpg ~ disp + wt, data = mtcars)
 #' most_deviant(df)
-#' @examples
+#' @export
 most_deviant <- function(lmobject){
   if(class(lmobject) == "lm"){
     absresid <- sort(abs(residuals(lmobject)))
     return(absresid[length(absresid)])
+  }
+  else{
+    stop('Input into function is not of class "lm"')
+  }
+}
+
+#' Most overpredicted case
+#'
+#' @param lmobject Object generated with \code{\link[stats]{lm}}
+#'
+#' @return The most overpredicted case with the largest negative residual
+#' (the most negative residual).
+#' @importFrom stats lm residuals
+#' @examples
+#' df <- lm(mpg ~ disp + wt, data = mtcars)
+#' most_overpredicted(df)
+#' @export
+most_overpredicted <- function(lmobject){
+  if(class(lmobject) == "lm"){
+    resid <- sort(residuals(lmobject))
+    return(resid[1])
   }
   else{
     stop('Input into function is not of class "lm"')
