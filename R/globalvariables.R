99416150bd771d528a8393de2728c6bed660df55
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2020-03-29 17:26:45 +0200

    Added linebreak

--- R/globalvariables.R
+++ R/globalvariables.R
@@ -1,1 +1,2 @@
-utils::globalVariables(c("df", "df_full", "df_reduced", "fit", "lwr", "outcome", "status", "upr"))
+utils::globalVariables(c("df", "df_full", "df_reduced", "fit", "lwr",
+                         "outcome", "status", "upr"))
